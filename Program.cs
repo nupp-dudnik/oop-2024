f7c0661fee946d9bb0fe036282c6c1b4337901e1
Viktor Dudnik <37380849+0x07C0@users.noreply.github.com>
2024-03-29 07:23:01 +0200

    feat: full lab19 impl with linq

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var text = "This lab is about working with strings and how to manipulate strings. " +
+    "First task is to simply count repeating words in a string and print thier occurence. " +
+    "Second task is to remove all words from this string.";
+
+var repeating_words = string.Join("", text.Select(c => char.IsPunctuation(c) ? ' ' : c)).Split(' ').Where(s => s.Length > 0)
+    .GroupBy(w => w).Where(o => o.Count() > 1).ToDictionary(x => x.Key, y => y.Count());
+Console.WriteLine($"Repeating words:\n - {string.Join("\n - ", repeating_words.Select(rw => $"`{rw.Key}` repeats {rw.Value} times"))}");
+
+Console.Write("Enter word to erase: ");
+var word = Console.ReadLine();
+Console.WriteLine(text.Replace($" {word}", ""));
