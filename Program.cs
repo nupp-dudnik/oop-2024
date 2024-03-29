419e0c5c7ef3714e5abd7293710547d9856bc344
Viktor Dudnik <37380849+0x07C0@users.noreply.github.com>
2024-03-29 07:34:05 +0200

    feat: full lab20 impl

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿var a = 1;
+var b = -3;
+var c = 2;
+
+try
+{
+    var roots = SolveQuadraticEquation(a, b, c);
+    Console.WriteLine($"Equation roots: {string.Join(", ", roots)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Failed solving the equation: {ex.Message}");
+}
+
+static double[] SolveQuadraticEquation(double a, double b, double c)
+{
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0)
+    {
+        var root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        var root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+        return [root1, root2];
+    }
+    else if (discriminant == 0)
+    {
+        var root = -b / (2 * a);
+        return [root];
+    }
+    else
+    {
+        throw new Exception("Equation doesn't have solutions");
+    }
+}
